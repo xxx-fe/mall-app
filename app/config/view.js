1a20812b0f9b0295b419920be0c55a164f2d98ff
Kevin <21108589@qq.com>
2018-01-22 22:22:20 +0800

    view.js

---
+++ app/config/view.js
@@ -0,0 +1,13 @@
+import views from 'koa-views';
+import path from 'path';
+/**
+ * 模板引擎
+ */
+export const view = async (app) => {
+    app.use(views(path.join(path.resolve('./app/view')), {
+        extension: 'hbs',
+        map: {
+            hbs: 'handlebars'
+        }
+    }));
+}
