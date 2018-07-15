af1afc6be22819261b633ab5c19e4e462037e712
Kevin <21108589@qq.com>
2018-07-16 02:04:00 +0800

    升级koa-webapck

--- server/lib/middleware/webpack.js
+++ server/lib/middleware/webpack.js
@@ -9,19 +9,21 @@ module.exports.default = module.exports = async (app) => {
         const webpack = require("webpack");
         const webpackConf = require(path.resolve('./build/webpack.dev.conf'));
         const compiler = webpack(webpackConf);
-        app.use(koaWebpack({
-            compiler: compiler,
-            hot: {
-                reload: true
-            },
-            dev: {
-                lazy: false,
-                watchOptions: {
-                    aggregateTimeout: 1200,
-                    poll: true
+        koaWebpack(
+            {
+                compiler: compiler,
+                devMiddleware: {
+                    publicPath: '/',
+                    logLevel: 'silent',
                 },
-                logLevel: 'silent'
+                hotClient: {
+                    logLevel: 'silent',
+                }
+
             }
-        }));
+        )
+            .then((middleware) => {
+                app.use(middleware);
+            });
     }
 };
