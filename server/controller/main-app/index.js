676e2cd006f3e6f42d50d32abe9d7002ebbf0d75
chenchangwen <21108589@qq.com>
2018-10-26 11:13:50 +0800

    注释

--- server/controller/main-app/index.js
+++ server/controller/main-app/index.js
@@ -4,13 +4,14 @@ const pageHome = async (ctx, _next) => {
     let locals = {
         title: 'home-page'
     };
-    //appName开发模式下不会加载生产后的css
+    //appName开发模式下不会加载生产后的css,只有在路由对应的控制器设置
     ctx.state.appName = 'main-app';
     await ctx.render('pages/main-app/home', locals);
 };
 
 
 const list = async (ctx, _next) => {
+    //不需要设置ctx.state.appName
     let locals = {
         list: await mainService.getList(ctx)
     };
